e6aa5dd35cce403dd177bfcf63c6c7cb83207e47
Mahamudurkhan10 <mahamudurkhan10@gmail.com>
2024-08-17 22:36:07 +0600

    collection add

--- index.js
+++ index.js
@@ -22,7 +22,11 @@ async function run() {
           // Connect the client to the server	(optional starting in v4.7)
           // await client.connect();
           // Send a ping to confirm a successful connection
-        
+          const productsCollection =client.db('shop-easy').collection('products')
+          app.get('/products',async(req,res)=>{
+               const result = await productsCollection.find().toArray();
+               
+          })
           await client.db("admin").command({ ping: 1 });
           console.log("Pinged your deployment. You successfully connected to MongoDB!");
      } finally {
