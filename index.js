43936000d53bfce6676bfa4e412e48c8d521e81b
Mahamudurkhan10 <mahamudurkhan10@gmail.com>
2024-08-17 22:44:17 +0600

    mongodb  add

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const cors = require('cors');
+require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
 const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://<username>:<password>@atlascluster.6gwdl3v.mongodb.net/?retryWrites=true&w=majority&appName=AtlasCluster";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@atlascluster.6gwdl3v.mongodb.net/?retryWrites=true&w=majority&appName=AtlasCluster`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -25,7 +26,7 @@ async function run() {
           const productsCollection =client.db('shop-easy').collection('products')
           app.get('/products',async(req,res)=>{
                const result = await productsCollection.find().toArray();
-               
+               res.send(result)
           })
           await client.db("admin").command({ ping: 1 });
           console.log("Pinged your deployment. You successfully connected to MongoDB!");
