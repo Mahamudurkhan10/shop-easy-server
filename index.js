7a752d492873951696c139f132eb01763f3f0c79
Mahamudurkhan10 <mahamudurkhan10@gmail.com>
2024-08-19 00:43:28 +0600

    code sendign

--- index.js
+++ index.js
@@ -3,7 +3,10 @@ const cors = require('cors');
 require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
-app.use(cors());
+app.use(cors({
+     origin: ['http://localhost:5173', 'http://localhost:5174'],
+     credentials: true
+}));
 app.use(express.json());
 
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
