347d9f395fbb88863d1f44c9f1e507b2feaad75a
Mahamudurkhan10 <mahamudurkhan10@gmail.com>
2024-08-19 01:07:20 +0600

    finish

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
 app.use(cors({
-     origin: ['http://localhost:5173', 'http://localhost:5174'],
+     origin: ['http://localhost:5173', 'http://localhost:5174','https://shop-easy-b1c31.web.app','https://shop-easy-b1c31.firebaseapp.com/'],
      credentials: true
 }));
 app.use(express.json());
