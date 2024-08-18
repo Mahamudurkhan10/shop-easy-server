12eee012317df0ba13c760733f09546e377c6e86
Mahamudurkhan10 <mahamudurkhan10@gmail.com>
2024-08-18 22:39:58 +0600

    product details

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@atlascluster.6gwdl3v.mongodb.net/?retryWrites=true&w=majority&appName=AtlasCluster`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -70,6 +70,12 @@ async function run() {
 
                res.send({ products, totalPages });
           });
+          app.get('/products/:id',async(req,res) =>{
+               const id = req.params.id;
+               const query = {_id : new ObjectId(id)}
+               const result = await productsCollection.findOne(query)
+               res.send(result)
+          })
 
           await client.db("admin").command({ ping: 1 });
           console.log("Pinged your deployment. You successfully connected to MongoDB!");
